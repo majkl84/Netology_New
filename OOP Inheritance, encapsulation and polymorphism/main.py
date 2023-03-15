43d7dc3361ec101f6de061e7e4833a62a0ea6936
majkl84 <34282210+majkl84@users.noreply.github.com>
2023-03-15 21:44:52 +0300

    Доработка  функций средней оценки

--- OOP Inheritance, encapsulation and polymorphism/main.py
+++ OOP Inheritance, encapsulation and polymorphism/main.py
@@ -9,21 +9,13 @@ class Student:
         self.grades = {}
         Student.student_list.append(self)
 
-
-    def grades(self):
-        sum_grades = []
-        for value_item in self.grades.values():
-            for new_list in value_item:
-                sum_grades += [new_list]
-
-        return sum_grades
-
-    def average_student(self):
-        try:
-            average_grade = sum(Student.grades(self)) / len(Student.grades(self))
-            return round(average_grade, 1)
-        except ZeroDivisionError:
-            print("Нет оценок")
+    def average(self):
+        if not self.grades:
+            return 0
+        estimation = []
+        for i in self.grades.values():
+            estimation.extend(i)
+        return round(sum(estimation) / len(estimation), 2)
     def rate_lec(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and \
                 course in lecturer.courses_attached and course in self.courses_in_progress and 1 <= grade <= 10:
@@ -34,31 +26,26 @@ class Student:
         else:
             return "Ошибка"
     def __str__(self):
-       print(
-                f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {Student.average_student(self)}"
-                f"\nКурсы в процессе изучения: {','.join(self.courses_in_progress)}\nКурсы завершены: {','.join(self.finished_courses)}")
+       return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {Student.average(self)}" \
+              f"\nКурсы в процессе изучения: {','.join(self.courses_in_progress)}\nКурсы завершены: {','.join(self.finished_courses)}"
 
     def __lt__(self, other):
         if not isinstance(other, self.__class__):
             raise Exception('...')
-        return (self.average_student()).__lt__(other.average_student())
+        return (self.average()).__lt__(other.average())
     def __le__(self, other):
         if not isinstance(other, self.__class__):
             raise Exception('...')
-        return (self.average_student()).__le__(other.average_student())
+        return (self.average()).__le__(other.average())
     def __eq__(self, other):
         if not isinstance(other, self.__class__):
             raise Exception('...')
-        return (self.average_student()).__eq__(other.average_student())
-
-
+        return (self.average()).__eq__(other.average())
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
-
-
 class Lecturer(Mentor):
     lecturer_list = []
     def __init__(self, name, surname):
@@ -66,46 +53,32 @@ class Lecturer(Mentor):
         self.grades = {}
         super().__init__(name, surname)
 
-    def grades(self):
-        sum_grades = []
-        for value_item in self.grades.values():
-            for new_list in value_item:
-                sum_grades += [new_list]
-
-        return sum_grades
-
     def average(self):
-        try:
-            average_grade = sum(Lecturer.grades(self)) / len(Lecturer.grades(self))
-            return round(average_grade, 1)
-        except ZeroDivisionError:
-            print("Нет курсов")
-
+        if not self.grades:
+            return 0
+        estimation = []
+        for i in self.grades.values():
+            estimation.extend(i)
+        return round(sum(estimation) / len(estimation), 2)
     def __lt__(self, other):
         if not isinstance(other, self.__class__):
             raise Exception('...')
         return (self.average()).__lt__(other.average())
-
     def __le__(self, other):
         if not isinstance(other, self.__class__):
             raise Exception('...')
         return (self.average()).__le__(other.average())
-
     def __eq__(self, other):
         if not isinstance(other, self.__class__):
             raise Exception('...')
         return (self.average()).__eq__(other.average())
-
     def __str__(self):
-        print(f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {Lecturer.average(self)}")
-
+        return f"Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {Lecturer.average(self)}"
 class Reviewer(Mentor):
-
     def __init__(self, name, surname):
         super().__init__(name, surname)
     def __str__(self):
         return f"Имя: {self.name}\nФамилия: {self.surname}"
-
     def rate_st(self, student, course, grade):
         if isinstance(student, Student) and \
                 course in self.courses_attached and course in student.courses_in_progress and 1 <= grade <= 10:
@@ -115,7 +88,6 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-
 def courses_average(persons, course):
     if not isinstance(persons, list):
         return "Нет студента"
@@ -168,13 +140,14 @@ ivan_student.rate_lec(Gena_lecturer, 'Java', 6)
 print(ruoy_student.grades)
 print(Some_mentor.courses_attached)
 print(Dima_lecturer.grades)
+print(Dima_lecturer)
+print(ruoy_student)
 
 print(f"Cредняя оценка за домашние задания Студентов: {courses_average(Student.student_list, 'Python')}")
 print(f"Cредняя оценка Лекторов: {courses_average(Lecturer.lecturer_list, 'Python')}")
 
 print(Dima_lecturer < Gena_lecturer)
-# print(Dima_lecturer <= Gena_lecturer)
+print(Dima_lecturer <= Gena_lecturer)
 print(Dima_lecturer == Gena_lecturer)
-print(ruoy_student < Gena_lecturer)
-
+print(ruoy_student == Dima_lecturer)
 print(ruoy_student < ivan_student)
