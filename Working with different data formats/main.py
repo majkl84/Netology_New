bb8507714509ca22e190e0e4aa5f5f76c3e3300d
majkl84 <34282210+majkl84@users.noreply.github.com>
2023-03-19 10:20:46 +0300

    Квиз 9 лекция

---
+++ Working with different data formats/main.py
@@ -0,0 +1,16 @@
+import json
+import collections
+
+def read_json(newsafr, max_len_word=6, top_words=10):
+    with open(newsafr, encoding='utf-8') as news_file:
+        news = json.load(news_file)
+        description_words = []
+        for item in news['rss']['channel']['items']:
+            description = [word for word in item['description'].split(' ') if len(word) > max_len_word]
+            description_words.extend(description)
+            counter_words = collections.Counter(description_words)
+        print(counter_words.most_common(top_words))
+
+if __name__ == '__main__':
+    read_json('newsafr.json')
+
