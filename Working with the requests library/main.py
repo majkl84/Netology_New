665841913ffe0730022917ddd3b5030168ec4130
majkl84 <34282210+majkl84@users.noreply.github.com>
2023-03-22 15:12:06 +0300

    Update main.py

--- Working with the requests library/main.py
+++ Working with the requests library/main.py
@@ -1,6 +1,7 @@
 import requests
 
 hero_list = ['Hulk', 'Captain America', 'Thanos']
+
 # def gero_intelligence(hero_person):
 #     hero_intelligence = {}
 #     url = 'https://akabab.github.io/superhero-api/api'
@@ -13,11 +14,13 @@ hero_list = ['Hulk', 'Captain America', 'Thanos']
 #     smartest_hero = max(hero_intelligence, key=hero_intelligence.get)
 #     return f'Самый умный супергерой - это {smartest_hero} с интеллектом {hero_intelligence[smartest_hero]}.'
 #
+#
 # print(gero_intelligence(hero_list))
 class YaUploader:
     def __init__(self, token):
         self.token = token
         self.headers = {"Authorization": f"OAuth {self.token}"}
+
     def upload(self, file_path: str):
         url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
         params = {"path": file_path}
@@ -28,6 +31,7 @@ class YaUploader:
             upload_url = response.json().get("href")
             response = requests.put(upload_url, headers=self.headers, data=file_path)
             return response.status_code == 201
+
 
 if __name__ == '__main__':
     # Получить путь к загружаемому файлу и токен от пользователя
